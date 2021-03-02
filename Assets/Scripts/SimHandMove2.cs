e8579054dffac964c4468f2dffb83ff27f534abb
viviancan <vcnales@gmail.com>
2021-03-01 18:47:13 -0600

    slight refactor of simHandMove & added additional scene for UI canvas

--- Assets/Scripts/SimHandMove2.cs
+++ Assets/Scripts/SimHandMove2.cs
@@ -4,70 +4,40 @@ using UnityEngine;
 
 public class SimHandMove2 : MonoBehaviour
 {
-    public Transform location;
+    public Transform location; //"location" game object
     public Vector3 position;
     public float moveSpeed;
     public float turnSpeed;
+    private float sprintFactor = 8;
 
     // Start is called before the first frame update
     void Start()
     {
         print("game starting");
         print(gameObject.name);
+        print("location: " + location.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (Input.GetKey(KeyCode.UpArrow) | Input.GetKey(KeyCode.W))
-        {
-            ResetMoveSpeed();
-
-            if (Input.GetKey(KeyCode.RightShift))
-            {
-                Sprint();
-            }
-
+        { 
             DoTranslation(Vector3.forward, moveSpeed);
         }
 
-
         if (Input.GetKey(KeyCode.DownArrow) | Input.GetKey(KeyCode.S))
         {
-            ResetMoveSpeed();
-
-            if (Input.GetKey(KeyCode.RightShift))
-            {
-                Sprint();
-            }
-
             DoTranslation(Vector3.back, moveSpeed);
-
         }
 
         if (Input.GetKey(KeyCode.LeftArrow) | Input.GetKey(KeyCode.A))
         {
-
-            ResetMoveSpeed();
-
-            if (Input.GetKey(KeyCode.RightShift))
-            {
-                Sprint();
-            }
-
             DoTranslation(Vector3.left, moveSpeed);
         }
 
         if (Input.GetKey(KeyCode.RightArrow) | Input.GetKey(KeyCode.D))
         {
-            ResetMoveSpeed();
-
-            if (Input.GetKey(KeyCode.RightShift))
-            {
-                Sprint();
-            }
-
             DoTranslation(Vector3.right, moveSpeed);
         }
 
@@ -100,12 +70,27 @@ public class SimHandMove2 : MonoBehaviour
                 DoRotation(Vector3.right, turnSpeed);
             }
         }
+
+        //For sprinting
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            Sprint();
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightShift))
+        {
+            ResetMoveSpeed();
+        }
     }
 
     void Sprint()
     {
         //https://answers.unity.com/questions/616090/sprint-script-for-fps.html
-        moveSpeed = 80.0f;
+        moveSpeed *= sprintFactor;
+    }
+    void ResetMoveSpeed()
+    {
+        moveSpeed = 10.0f;
     }
 
     void DoTranslation(Vector3 direction, float speed)
@@ -118,8 +103,4 @@ public class SimHandMove2 : MonoBehaviour
         transform.Rotate(direction * Time.deltaTime * speed, Space.Self);
     }
 
-    void ResetMoveSpeed()
-    {
-        moveSpeed = 10.0f;
-    }
 }
