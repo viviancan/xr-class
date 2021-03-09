4d5f08392f99c95ffff53ef41665b0b1c48b3752
viviancan <vcnales@gmail.com>
2021-03-08 18:46:48 -0600

    added VR Hand pack and script for opening and closing hand

---
+++ Assets/Scripts/SimHandAnimator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimHandAnimator : MonoBehaviour
+{
+    private Animator simHandAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // filling the "bucket" line 7
+        simHandAnimator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            SetAnimator(true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            SetAnimator(false);
+        }
+    }
+
+    void SetAnimator(bool booleanValue)
+    {
+        simHandAnimator.SetBool("isClosing", booleanValue);
+    }
+}
